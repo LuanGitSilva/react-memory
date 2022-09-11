ac65cfebb52e690dd4ec53841665c06ade6b4122
Luan Silva <luan_barbosa_da_silva@outlook.com>
2022-09-11 02:40:56 -0300

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,16 @@
+import android from '../svgs/android.svg'; // importando imagem para inserir no icon do item.
+import caminhao from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import estrela from '../svgs/estrela.svg';
+import gasolina from '../svgs/gasolina.svg';
+import moto from '../svgs/moto.svg';
+
+// criando um array com os itens, e cada item será um objeto pois eles tem mais de uma informação cada.
+export const items = [
+    { name: 'android', icon: android },
+    { name: 'caminhao', icon: caminhao },
+    { name: 'disney', icon: disney },
+    { name: 'estrela', icon: estrela },
+    { name: 'gasolina', icon: gasolina },
+    { name: 'moto', icon: moto }
+];
